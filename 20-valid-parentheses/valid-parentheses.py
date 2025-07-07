7c415c363d5143ce093b0dd789dd64fa43f284c4
Vedang Gharat <89417414+VedangGharat@users.noreply.github.com>
2025-07-06 15:38:59 -1000

    Time: 0 ms (100.00%) | Memory: 17.7 MB (96.43%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        hmap = {")":"(", "]":"[", "}":"{"}
-        res = []
-        for c in s:
-            if c in hmap.values():
-                res.append(c)
-            elif res and res[-1] == hmap[c]:
-                
-                res.pop()
-            else:
-                return False
-        return res == []
+        hmap = {"]":"[", ")":"(", "}":"{"}
+        stack = []
+        for p in s:
+            if p in hmap.values():
+                stack.append(p)
+            elif stack and stack[-1] == hmap[p]:
+                stack.pop()
+            else: return False
+        return stack == []
