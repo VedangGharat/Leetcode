44a2b80d3147c0f7f9c03d0b115ffe057af69cad
Vedang Gharat <89417414+VedangGharat@users.noreply.github.com>
2025-08-19 17:06:18 -1000

    Time: 1 ms (25.84%) | Memory: 17.9 MB (55.43%) - LeetSync

--- 1391-counting-elements/counting-elements.py
+++ 1391-counting-elements/counting-elements.py
@@ -2,9 +2,6 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         hmap = collections.Counter(arr)
-        count = 0
-        for k, v in hmap.items():
-            if k+1 in hmap:
-                count += v
-        return count
+            
+        return sum(v for k, v in hmap.items() if k+1 in hmap)
                 
